a0f1fcf94f3cb2f33478d850f6e2947c954cec9a
atsilcock <143049271+atsilcock@users.noreply.github.com>
2024-08-24 17:58:49 -0600

    updating card

--- src/Components/PetCard.js
+++ src/Components/PetCard.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import "../App.css"
 
-const PetCard = () => {
+const PetCard = ({ breed }) => {
+    const [button, setButton] = useState(breed.isAdopted)
+
+function handleButton(){
+    setButton(!button)
+    fetch(`http://localhost:4000/dogBreeds/${breed.id}`,{
+        method: "PATCH", 
+        headers:{
+            "Content-type" : "application/json"
+        },
+        body: JSON.stringify({
+            isAdopted: true
+        })
+    })
+    .then(response => response.json())
+}
   return (
     <div>
       
