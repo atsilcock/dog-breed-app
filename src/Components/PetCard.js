66f8416f2deff5aa1b7dfc8857a371d93d2000dd
atsilcock <143049271+atsilcock@users.noreply.github.com>
2024-08-24 18:24:06 -0600

    updating Petcard

--- src/Components/PetCard.js
+++ src/Components/PetCard.js
@@ -1,38 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "../App.css"
 
 const PetCard = ({ breed }) => {
-    const [button, setButton] = useState(breed.isAdopted)
+  const [button, setButton] = useState(breed.isAdopted)
 
 function handleButton(){
-    setButton(!button)
-    fetch(`http://localhost:4000/dogBreeds/${breed.id}`,{
-        method: "PATCH", 
-        headers:{
-            "Content-type" : "application/json"
-        },
-        body: JSON.stringify({
-            isAdopted: true
-        })
+  setButton(!button)
+  fetch(`http://localhost:4000/dogBreeds/${breed.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type" : "application/json"
+    }, 
+    body: JSON.stringify({
+      isAdopted: true
     })
-    .then(response => response.json())
+  })
+  .then(response => response.json())
+  
 }
+
+
   return (
     <div>
-      <img src={breed.photo} alt={breed.name} className="pet-image"></img>
-      <ul>
-        <li>
-            Name: {breed.name}
-            Breed: {breed.breed}
-            Breed Type: {breed.breedType}
-            Origin of Country: {breed.origin}
-            How Popular is {breed.Name}? (1-10): {breed.popularity}
-            Temperament: <li>{breed.temperament}</li>
-            Hypoallergenic: {breed.hypoallergenic}
-            Intelligence (1-100): {breed.intelligence}
-        </li>
-      </ul>
-      <button onClick={handleButton}>{button ? "Adopted" : "Not Adopted"} </button>
+     <img src={breed.photo} alt={breed.name} className="pet-image"></img>
+    <ul>
+      <li>
+      Name: {breed.name} <br />
+      Breed: {breed.breed} <br />
+      Breed Type: {breed.breedType} <br />
+      Origin of Country: {breed.origin} <br />
+      </li>
+    </ul>
+    <button onClick={handleButton}>{button ? "Adopted" : "Not Adopted"}</button>
+      
     </div>
   )
 }
