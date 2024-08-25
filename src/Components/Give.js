dc1c2a24889fa36f6f04cb7f86853e6ce6c3f339
atsilcock <143049271+atsilcock@users.noreply.github.com>
2024-08-24 18:08:06 -0600

    updating useStates

--- src/Components/Give.js
+++ src/Components/Give.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Give = () => {
+    const [name, setName] = useState("")
+    const [breed, setBreed] = useState("")
+    const [breedType, setBreedType] = useState("")
+    const [origin, setOrigin] = useState("")
+    const [popularity, setPopularity] = useState("")
+    const [temperament, setTemperament] = useState("")
+    
+
+
   return (
     <div>
       
