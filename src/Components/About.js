ef91b9de273647589621db094f167ec40a8bca95
atsilcock <143049271+atsilcock@users.noreply.github.com>
2024-08-24 18:22:29 -0600

    updating about.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,34 +1,36 @@
 import React, {useState} from 'react'
 import PetCard from './PetCard'
 
-const About = ({ breeds }) => {
-    const[search, setSearch] = useState("")
+const About  = ({ breeds }) => {
+  const[search, setSearch] = useState("")
   console.log(search)
+  
 
-  function handleSearch(event){
+  function handleSearch (event){
     setSearch(event.target.value)
   }
-
-   const filterBreeds = breeds.filter((breed) => {
-    return breed.origin.toLowerCase().includes(search.toLowerCase)
-   })
-   console.log(filterBreeds)
-
-   const mappedBreeds = filterBreeds.map((breed) => <PetCard key={breed.id} breed={breed} />)
+  
+  const filterBreeds = breeds.filter((breed) => {
+    return breed.origin.toLowerCase().includes(search.toLowerCase())
+  })
+  console.log(filterBreeds)
+  
+const mappedBreeds = filterBreeds.map((breed) => <PetCard key={breed.id} breed={breed}/>)
+  
 
   return (
     <div>
-      <h1>Let's find the Pet You would like to Adopt</h1>
+      <h1>Let's Find the Pet You want to Adopt</h1>
       <h2>Search by Country: USA, Canada, Germany, United Kingdom(UK), France</h2>
-      <lable>
+      <label>
         Search by Country
-      </lable>
+      </label>
       <input type="text" value={search} onChange={handleSearch}></input>
       <ul>
-        {mappedBreeds}
+      {mappedBreeds}
       </ul>
     </div>
   )
 }
 
-export default About
+export default About 
